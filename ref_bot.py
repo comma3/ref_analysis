0ae14eb69b430037e7a9bb86992a822477766dc9
Matthew Frederick <matthewfred@gmail.com>
2017-11-20 21:58:34 -0700

    More organization and simplification

--- ref_bot.py
+++ ref_bot.py
@@ -4,7 +4,6 @@ import sqlite3
 import re
 
 
-
 def determine_game_thread(threads):
     """
     Takes a list of potential gamethreads and returns the post that is
@@ -38,29 +37,41 @@ def analyze_comments(to_analyze):
     """
     pass
 
+def collect_old_game_ids(passed_set):
+    """
+    Gets list of games that have already been analyzed to ensure that games
+    aren't analyzed twice. While running, the set should be kept in memory
+    and passed around rather than touching the DB. Passing an empty set
+    will cause the DB query.
 
-# Make sure we aren't doubling up on games
-# Should use ESPN game id to ensure uniqueness
-# postgame threads are supposed to contain links to box scores,
+    INPUT: set
+
+    OUTPUT: set
+    """
+    if not passed_set:
+        conn = sqlite3.connect("D:\\reddit\\reddit_user_data.sqlite3")
+        curr = conn.cursor()
+        curr.execute("""SELECT username
+                        FROM users
+                        """)
+        old_games = curr.fetchall()
+        conn.close()
+        return set(old_games)
+
+    return set() # Returning an empty set for now
+
+
+# Make sure we aren't doubling up on games. Uses ESPN game id to ensure
+# uniqueness. Postgame threads are supposed to contain links to box scores,
 # so we will collect them from there.
-game_ids = set()
-# conn = sqlite3.connect("D:\\reddit\\reddit_user_data.sqlite3")
-# curr = conn.cursor()
-# curr.execute("""SELECT username
-#                 FROM users
-#                 """)
-# temp_games = curr.fetchall()
-# conn.close()c
-#
-# for game in temp_games:
-#     game_id.add(game[0])
+game_ids = collect_old_game_ids(True)
 
 # Create bot instance
 reddit = praw.Reddit('bot1')
 
 # Store game threads in case there are multiple threads for a game.
 # Clear this at some interval (maybe daily at like 4 am to limit maintanence).
-potential_valid_game_threads = []
+potential_valid_game_threads = {}
 
 # Find new posts in /r/CFB
 submissions = reddit.subreddit('CFB').new(limit=5)
@@ -79,101 +90,58 @@ for post in submissions:
     if not is_old_enough:
         continue
 
-
-    is_game_thread = '[game thread]' in post.lower()
-    is_postgame_thread ='[postgame thread]' in post.lower()
-
-    if is_game_thread:
+    if '[game thread]' in post.lower():
         if not post.is_self:
-            raise "Game thread was not a selfpost. Post id: {}".format(post.id)
+            raise "Game thread is not a selfpost. Post id: {}".format(post.id)
 
         away, home = re.findall(r'([A-z -]+) @ ([A-z -]+)'), post.lower())
-        potential_valid_game_threads[post.id] = (away, home)
+        try:
+            potential_valid_game_threads[(away, home)].append(post.id)
+        except KeyError:
+            potential_valid_game_threads[(away, home)] = [post.id]
 
-    # Check for postgame threads in new posts
-    # We are only going to act on postgame threads that are older than 5 minutes
-    # so that we ensure that we have the correct game
-    elif is_postgame_thread:
-        # We can start analyzing the game thread because commenting is effectly
-        # finished
+
+    elif '[postgame thread]' in post.lower():
         if not post.is_self:
             raise "Postgame thread is not a selfpost. Post id: {}".format(post.id)
 
-        try:
-            game_id = re.findall(r'gameId=([0-9]+)', post.selftext)[0]
-        except: # TODO: determine the error
-            raise "No ESPN link in postgame thread id {}".format(post.id)
+        game_id = re.findall(r'gameId=([0-9]+)', post.selftext)[0]
+        if game_id in game_ids:
+            raise "Game already analyzed. Post id: {}".format(post.id)
 
         winner, loser = re.findall(r'([A-z -]+) defeats ([A-z -]+)'), post.lower())
 
-        game_threads = [(thread, winner, loser) for thread, teams in potential_valid_game_threads.items() if away in teams and home in teams]
-
-        # Keep track of whether the winning team was home or away
-        home_winner = winner == gamethreads[0][1]
+        # Determining the winner and getting game thread list at the same time
+        # May be worth checking if both exist (i.e., someone made an
+        # incorrect game thread or post game thread. Five minue wait should
+        # prevent those threads from being found)
+        try:
+            game_threads = potential_valid_game_threads[(winner, loser)]
+            home_winner = False
+        except:
+            game_threads = potential_valid_game_threads[(loser, winner)]
+            home_winner = True
 
         if not game_threads:
             # Do a search back a few hours and try to find the game thread.
+            # Alternatively, may simply raise an exception
             pass
 
         # Generate list of games to analyze
         to_analyze.append(determine_game_thread(game_threads))
 
-        # Do some clean up.
-        # This should keep the potential_valid_game_threads list manageable.
-        # Problems may occur if a duplicate postgame thread appears long
-        # after the game ended and a  postgame thread was already found.
+        # Do some clean up to keep the potential_valid_game_threads
+        # list manageable. Problems may occur if a duplicate postgame thread
+        # appears long after the game ended and a postgame thread was already
+        # found. Unique checks using game_id should prevent problems.
         for thread in game_threads:
-            del potential_valid_game_threads[thread]
+            if home_winner:
+                del potential_valid_game_threads[(loser, winner)]
+            else:
+                del potential_valid_game_threads[(winner, loser)]
 
-        # Game_id, Game_thread_id, Post_gamethread_id, Winner, Loser, Winner_H
+        # Game_id, Game_thread_id, Post_gamethread_id, Winner, Loser, home_winner
 
 
 # We may want some parallelization here so we don't miss new threads
 analyze_comments(to_analyze)
-
-
-
-
-
-
-# # Just gonna create the table here so I don't have to do it elsewhere
-# # Pass if the table already exists
-# # This is just me being lazy and it's nice to see the table structure here
-# try:
-#     curr.execute("""CREATE TABLE
-#                     users (
-#                     username string,
-#                     gender string,
-#                     age int,
-#                     subreddits string
-#                     )
-#                     """)
-#     curr.execute("""CREATE TABLE
-#                     comments (
-#                     user string,
-#                     body string,
-#                     score int,
-#                     date string,
-#                     subreddit string
-#                     )
-#                     """)
-#
-# except sqlite3.OperationalError:
-#     pass
-
-
-
-# for key, value in users.items():
-#     user = (key, value[1], value[2], ','.join(value[3]))
-#     curr.execute("""INSERT INTO users
-#                 (username, gender, age, subreddits)
-#                 VALUES (?,?,?,?)
-#                 """, user)
-#     for comment in value[0]:
-#         curr.execute("""INSERT INTO comments
-#                     (user, body, score, date, subreddit)
-#                     VALUES (?,?,?,?,?)
-# #                     """, (key, comment[0], comment[1], comment[2], comment[3]))
-# conn.commit()
-# conn.close()
-# print('Added to DB!')
