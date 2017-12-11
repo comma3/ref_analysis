4d0b54de924c69db31efb57aa70959ea47913755
Matthew Frederick <matthewfred@gmail.com>
2017-12-11 14:20:10 -0700

    Search is still taking too long. updating for now but need to make some chagnes.

--- initial_data_collector.py
+++ initial_data_collector.py
@@ -6,7 +6,7 @@ import sqlite3
 
 import praw
 
-def collect_old_game_ids(passed_set):
+def collect_old_game_ids(db, passed_set=False):
     """
     Gets list of games that have already been analyzed to ensure that games
     aren't analyzed twice. While running, the set should be kept in memory
@@ -18,18 +18,31 @@ def collect_old_game_ids(passed_set):
     OUTPUT: set
     """
     if not passed_set:
-        conn = sqlite3.connect("D:\\reddit\\reddit_user_data.sqlite3")
+        conn = sqlite3.connect(db)
         curr = conn.cursor()
-        curr.execute("""SELECT username
-                        FROM users
+        curr.execute("""SELECT
+                        game_id
+                        FROM
+                        games
                         """)
         old_games = curr.fetchall()
+
+        curr.execute("""SELECT
+                        thread, date
+                        FROM
+                        games
+                        ORDER BY
+                        date
+                        LIMIT
+                        1
+                        """)
+        last_game = curr.fetchall()
         conn.close()
-        return set(old_games)
+        return last_game, set(old_games)
 
-    return set() # Returning an empty set for now
+    return 0, set() # Returning an empty set for now
 
-def call_get_submissions(praw_instance, start_time, total_list=[], paginate=False, subreddit='cfb'):
+def call_get_submissions(praw_instance, start_time, total_list=[], paginate=False, game_ids=set(), subreddit='cfb'):
     """
     Recursive approach rapidly exceeded max recursion depth.
 
@@ -90,7 +103,7 @@ def get_submissions(praw_instance, start_time, total_list=[], paginate=False, su
     return total_list, paginate
 
 
-def analyze_game_thread(threads):
+def analyze_game_thread(threads, old_games):
     """
     Takes a list of potential gamethreads and filters for duplicates.
 
@@ -136,21 +149,45 @@ def analyze_game_thread(threads):
             if away in working_dict:
                 output_dict[working_dict[away]] = [date, thread.id, home, away, away, 0]
             elif home in working_dict:
-                output_dict[working_dict[home]] = [date, thread.id, home, away, home, 0]
+                output_dict[work?,?,?ing_dict[home]] = [date, thread.id, home, away, home, 0]
             else:
                 print(thread)
                 raise "Postgame thread appeared before game thread!"
 
-    conn = sqlite3.connect("/data/cfb_game_db.sqlite3")
-    curr = conn.cursor()
-    curr.execute("""SELECT username
-                    FROM users
-                    """)
-    old_games = curr.fetchall()
-    conn.close()
+        update_db(output_dict)
 
 
+    def update_db(games):
 
+        conn = sqlite3.connect("/data/cfb_game_db.sqlite3")
+        curr = conn.cursor()
+
+        # Just gonna create the table here so I don't have to do it elsewhere
+        # Pass if the table already exists
+        try:
+            curr.execute("""CREATE TABLE
+                            games (
+                            game_id string, --Only an int if you want to do math with it
+                            date string,
+                            thread string,
+                            home string,
+                            away string,
+                            winner string,
+                            call_differential
+                            )
+                            """)
+
+        except sqlite3.OperationalError:
+            # Totally fine that table already exists
+            pass
+
+        game_tuples = (k,v for k,v in games.items())
+        curr.executemany("""INSERT INTO playbyplay
+                        (game_id, date, thread, home, away, winner, call_differential)
+                        VALUES (?,?,?,?,?,?,?)
+                        """, game_tuples)
+
+        conn.close()
 
 
 def analyze_comments(to_analyze):
@@ -163,102 +200,25 @@ def analyze_comments(to_analyze):
 
 
 
-# Make sure we aren't doubling up on games. Uses ESPN game id to ensure
-# uniqueness. Postgame threads are supposed to contain links to box scores,
-# so we will collect them from there.
-game_ids = collect_old_game_ids(True) # Currently just passing until db is set up
+if __name__ == '__main__':
+    # Make sure we aren't doubling up on games. Uses ESPN game id to ensure
+    # uniqueness. Postgame threads are supposed to contain links to box scores,
+    # so we will collect them from there.
+    last_game, game_ids = collect_old_game_ids(True)  # Currently just passing until db is set up
 
-# Create bot instance
-reddit = praw.Reddit('bot1')
+    # Create bot instance
+    reddit = praw.Reddit('bot1')
 
-# Store game threads in case there are multiple threads for a game.
-# Clear this at some interval (maybe daily at like 4 am to limit maintanenceS).
-potential_valid_game_threads = {}
+    # In case program crashes while collecting,
+    # we find (one of) the oldest found games and start our
+    # search there.
+    if last_game:
+        paginate = reddit.submission(id=last_game)
+    else:
+        paginate = False
 
-search_depth = datetime.utcnow() - timedelta(weeks=4)
-time_start = datetime.now()
-results = call_get_submissions(reddit, search_depth)
-print(datetime.now() - time_start)
+    search_depth = datetime.utcnow() - timedelta(weeks=4)
 
-
-
-print(results)
-
-
-
-
-
-# # Find new posts in /r/CFB
-# submissions = reddit.subreddit('CFB').new(limit=5)
-# for post in submissions:
-#     # First we need to find the correct game thread in case multiple threads
-#     # are created. I believe duplicates are usually deleted quite quickly,
-#     # so 5 minutes should be plenty. We'll count posts in case there are
-#     # multiple game threads to differentiate.
-#
-#     is_old_enough = post.created_utc < time.time() - 300:
-#     # This old enough metric may not work if posts are created too quickly
-#     # That is, they might not be returned by subreddit.new() after 5 minutes
-#
-#     # Checking age first and continuing prevents excess nesting and improves
-#     # readability.
-#     if not is_old_enough:
-#         continue
-#
-#     if '[game thread]' in post.lower():
-#         if not post.is_self:
-#             raise "Game thread is not a selfpost. Post id: {}".format(post.id)
-#
-#         away, home = re.findall(r'([A-z -]+) @ ([A-z -]+)'), post.lower())
-#         try:
-#             potential_valid_game_threads[(away, home)].append(post.id)
-#         except KeyError:
-#             potential_valid_game_threads[(away, home)] = [post.id]
-#
-#
-#     elif '[postgame thread]' in post.lower():
-#         if not post.is_self:
-#             raise "Postgame thread is not a selfpost. Post id: {}".format(post.id)
-#
-#         game_id = re.findall(r'gameId=([0-9]+)', post.selftext)[0]
-#         if game_id in game_ids:
-#             raise "Game already analyzed. Post id: {}".format(post.id)
-#
-#         winner, loser = re.findall(r'([A-z -]+) defeats ([A-z -]+)'), post.lower())
-#
-#         # Determining the winner and getting game thread list at the same time
-#         # May be worth checking if both exist (i.e., someone made an
-#         # incorrect game thread or post game thread. Five minue wait should
-#         # prevent those threads from being found)
-#         try:
-#             game_threads = potential_valid_game_threads[(winner, loser)]
-#             home_winner = False
-#         except:
-#             game_threads = potential_valid_game_threads[(loser, winner)]
-#             home_winner = True
-#
-#         if not game_threads:
-#             # Do a search back a few hours and try to find the game thread.
-#             # Alternatively, may simply raise an exception
-#             pass
-#
-#         # Generate list of games to analyze
-#         to_analyze.append(determine_game_thread(game_threads))
-#
-#         # Do some clean up to keep the potential_valid_game_threads
-#         # list manageable. Problems may occur if a duplicate postgame thread
-#         # appears long after the game ended and a postgame thread was already
-#         # found. Unique checks using game_id should prevent problems.
-#         for thread in game_threads:
-#             if home_winner:
-#                 del potential_valid_game_threads[(loser, winner)]
-#             else:
-#                 del potential_valid_game_threads[(winner, loser)]
-#
-#         # Game_id, Game_thread_id, Post_gamethread_id, Winner, Loser, home_winner
-#
-#         # Game_thread_id, post_id, post_body
-#
-#
-# # We may want some parallelization here so we don't miss new threads
-# analyze_comments(to_analyze)
+    time_start = datetime.now()
+    call_get_submissions(reddit, search_depth, paginate=paginate, game_ids = game_ids) # Adds to DB
+    print(datetime.now() - time_start)
