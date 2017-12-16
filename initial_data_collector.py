3038e7c2506322e42ae012c500162356164298a2
Matthew Frederick <matthewfred@gmail.com>
2017-12-16 14:35:15 -0700

    simplified game title cleaner

--- initial_data_collector.py
+++ initial_data_collector.py
@@ -9,15 +9,13 @@ import sqlite3
 
 import praw
 
-def clean_team_names(title, is_postgame):
+def clean_team_names(title):
     """
     The format of game thread titles is variable. This function attempts to
     handle all of the formats. It may need to be adjusted for other sports.
 
     INPUTS:
     title: Str title of game thread
-    is_postgame: bool indicating whether the match should be for game thread or
-                postgame thread
 
     OUTPUT:
     tuple of cleaned team names
@@ -25,29 +23,33 @@ def clean_team_names(title, is_postgame):
     """
     # probably a better way to do this
     # vs. indicates neutral site. May alter stats somewhat.
-    #away, home = map(str.strip, re.findall(r'\]([A-z \-&\.\']+) [@|vs.]+ ([A-z \-&\.\']+)', current_title)[0])
-    #away, home = map(str.strip, re.findall(r'[\]|:|\)](\D+) [@|vs.|at]+ (\D+)[ (]', current_title)[0])
 
     title = title.replace(u'\xe9','e') # replaces e` in san jose
 
     # split based on type of title
-    if is_postgame:
-        try:
-            first, second = title.split('defeats')
-        except ValueError:
-            # very rare
-            try:
-                first, second = title.split('beats')
-            except ValueError:
-                print('Someone thought they were clever with this title: {}'.format(title))
-                first = ']clever'
-                second = 'asgd'
-        first = first.split(']')[1]
-        #first, second = re.findall(r'[\]](\D+) defeats (\D+)', title)[0]
-    else:
-        first, second = re.findall(r'[\]]([\s\S]+) [@|vs.|at]+ ([\s\S]+)[:]{0,1}', title)[0]
+    # Change the regex to find these
+    # if is_postgame:
+    #     if 'defeats' in title:
+    #         first, second = title.split('defeats')
+    #     elif 'defeat' in title:
+    #         first, second = title.split('defeats')
+    #     elif 'beats' in title:
+    #         first, second = title.split('beats')
+    #     elif 'beat' in title:
+    #         first, second = title.split('beats')
+    #     else:
+    #         # Gets caught outside function
+    #         raise IndexError
+    #     # Drop '[(post)game thread]'
+    #     first = first.split(']')[1]
+    # else:
+    first, second = re.findall(r'[\]]([\s\S]+) (?:@|[vs.]+|at|defeat[s]*|beat[s]*|upset[s]*|survive[s]*) ([\s\S]+)', title)[0]
     # Some games have titles (e.g., FCS Championship: ) and are split on ':'
-    # I like the EAFP approach rather than LBYL
+    # First we need to get rid of time (e.g., 6:30) - just remove :[0-9]
+    for i in range(6):
+        first = first.replace(':' + str(i),'')
+        second = second.replace(':' + str(i),'')
+    # Then, we split on the colon. I like the EAFP approach rather than LBYL
     try:
         first = first.split(':')[1]
     except IndexError:
@@ -56,6 +58,13 @@ def clean_team_names(title, is_postgame):
         second = second.split(':')[1]
     except IndexError:
         pass
+
+    # Sometimes we would end up with only spaces from titles like
+    # [post game thread] #18 ucla defeats washington 44-30 from the split on
+    # ']' with a space before #
+    first = first.strip()
+    second = second.strip()
+
     first_clean = re.findall(r'([a-z \-&\.\']+)', first)[0]
     second_clean = re.findall(r'([a-z \-&\.\']+)', second)[0]
     return first_clean.strip(), second_clean.strip()
@@ -120,7 +129,7 @@ def generate_dates(season_start, season_end, year, interval=86400):
 
     # Check if we span new years
     season_start += '/' + str(year)
-    if season_end.split('/')[0] <= season_start.split('/')[0]:
+    if season_end.split('/')[0] < season_start.split('/')[0]:
         season_end += '/' + str(year + 1)
     else:
         season_end += '/' + str(year)
@@ -172,7 +181,7 @@ def get_submissions(praw_instance, start_time, stop_time=None, query='', subredd
     return game_threads
 
 
-def analyze_game_thread(threads, old_games, db):
+def analyze_game_thread(threads, old_games):
     """
     Takes a list of potential game_threads and filters for duplicates.
 
@@ -205,10 +214,11 @@ def analyze_game_thread(threads, old_games, db):
                 # Probably want a better way to handle these
                 print("No game id in postgame thread!\nTitle: {}\nThread: {}".format(current_title, thread))
                 continue
-                #raise BaseException("No game id in postgame thread!\nTitle: {}\nThread: {}".format(current_title, thread))
             try:
-                winner, loser = clean_team_names(current_title, True)
+                winner, loser = clean_team_names(current_title)
             except IndexError:
+                # Print these threads for review
+                # Most of the time they are fine.
                 print('Incorrect postgame thread format')
                 print(current_title)
                 print(thread)
@@ -222,19 +232,25 @@ def analyze_game_thread(threads, old_games, db):
                 no_game_thread.append(working_dict[winner])
                 # Overwrite the previous entry and move on.
                 working_dict[winner] = game_id
+            elif not winner:
+                # This issue should be resolved
+                print('+++++++++++\nNo winner returned!\n+++++++++')
+                print(loser)
+                print(current_title)
+                print(thread)
             else:
                 working_dict[winner] = game_id
 
         elif '[game thread]' in current_title:
             try:
-                away, home = clean_team_names(current_title, False)
+                away, home = clean_team_names(current_title)
             except IndexError:
                 print('Incorrect game thread format')
                 print(current_title)
                 print(thread)
                 continue
 
-            date = thread.created
+            date = datetime.fromtimestamp(thread.created).strftime('%Y-%m-%d')
             if (date, home, away) in found:
                 raise BaseException("Duplicate game threads! Thread id: {}".format(thread))
             found.add((date, home, away))
@@ -249,27 +265,21 @@ def analyze_game_thread(threads, old_games, db):
             else:
                 # Probably just pass here. Occasionally game threads aren't created
                 # if we don't have a game thread, we can't really do anything.
-                # print('postgame thread not found')
-                # print(home, away)
-                # #print(working_dict.keys())
-                # print(current_title)
-                # print(thread)
                 no_post_game.append((home, away, thread.id))
-                #raise BaseException("Postgame thread appeared before game thread! Thread id: {}".format(thread))
     #print(len(output_dict))
     #print(len(no_post_game))
     #print(no_post_game)
     #print(len(no_game_thread))
-    #print(len(output_dict))
-    update_db(output_dict, db)
-
-
+    print("Number of games found in this interval: ", len(output_dict))
+    return output_dict
 
 
 def update_db(games, db):
-
+    """
+    Takes dictionary of games and adds to the db
+    """
     # need to check for existence of folder and make it
-    conn = sqlite3.connect("/data/cfb_game_db.sqlite3")
+    conn = sqlite3.connect(db)
     curr = conn.cursor()
     # Just gonna create the table here so I don't have to do it elsewhere
     # Pass if the table already exists
@@ -287,22 +297,22 @@ def update_db(games, db):
                         approx_home_fans int, -- maybe detractors?
                         approx_away_fans int,
                         approx_impartial_fans int
-                        )
+                        );
                         """)
+        conn.commit()
 
     except sqlite3.OperationalError:
         # Totally fine that table already exists
         pass
 
     # Flatten dictionary results into a single list
-    game_tuples =[]
+    game_list =[]
     for k,v in games.items():
         temp = [k]
         for x in v:
             temp.append(x)
-        game_tuples.append(temp)
+        game_list.append(temp)
 
-    print(game_tuples)
     curr.executemany("""INSERT INTO games
                     (game_id,
                     date,
@@ -316,9 +326,10 @@ def update_db(games, db):
                     approx_away_fans,
                     approx_impartial_fans)
                     VALUES
-                    (?,?,?,?,?,?,?,?,0,0,0,0)
-                    """, game_tuples)
+                    (?,?,?,?,?,?,?,0,0,0,0);
+                    """, game_list)
 
+    conn.commit()
     conn.close()
 
 
@@ -328,8 +339,8 @@ if __name__ == '__main__':
     # Could make this a command line script
     db = '~/data/cfb_game_db.sqlite3'
     subreddit = 'cfb'
-    season_start ='10/29' # 'Month/Day' requires /
-    season_end = '11/9'
+    season_start ='9/1' # 'Month/Day' requires /
+    season_end = '9/17'
     # Postgame threads didn't really start appearing until 2014
     # In the future, I'd like to make search more flexible.
     first_year = 2014 # Parser should accept both 20XX and XX
@@ -347,6 +358,7 @@ if __name__ == '__main__':
     time_start = datetime.now()
 
     for year in range(first_year, last_year+1):
+        print('==========START {}============='.format(year))
         game_threads = []
         for date in generate_dates(season_start, season_end, year):
             #print(date) # UTC
@@ -355,6 +367,8 @@ if __name__ == '__main__':
             # Determine size in memory -> ~100 MB / CFB season
             size = sum([sys.getsizeof(x) for x in game_threads])
             print('List size: ', len(game_threads), ' Memory ', size)
+        print('==========THREADS FOR {}============='.format(year))
         # Could do some parallelization here
-        analyze_game_thread(game_threads, game_ids, db) # Adds to DB
+        valid_threads = analyze_game_thread(game_threads, game_ids)
+        update_db(valid_threads, db)
     print(datetime.now() - time_start)
