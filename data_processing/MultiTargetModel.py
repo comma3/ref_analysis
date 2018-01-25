fixed recall and precision calculations

data_processing/MultiTargetModel.py
@@ -7,6 +7,8 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import MultinomialNB
 
+from sklearn.model_selection import train_test_split
+
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 from library import *
@@ -15,10 +17,11 @@ from LemmaTokenizer import LemmaTokenizer
 class MultiTargetModel():
     """
     """
-    def __init__(self, model, n_jobs=1, vectorizer=CountVectorizer, stop_words='english', tokenizer=LemmaTokenizer):
+    def __init__(self, model, n_jobs=1, vectorizer=CountVectorizer, stop_words='english', tokenizer=LemmaTokenizer, **vectorizer_params):
         self.model = model
         self.n_jobs = n_jobs
-        self.vectorizer = vectorizer(stop_words=stop_words, tokenizer=tokenizer())
+        self.mlb = None # MultiLabelBinarizer
+        self.vectorizer = vectorizer(stop_words=stop_words, tokenizer=tokenizer(), **vectorizer_params)
         self.tokenizer = tokenizer
         self.stop_words = stop_words
 
@@ -37,7 +40,12 @@ class MultiTargetModel():
         """
         self.X = self.vectorizer.fit_transform(X)
         self.classifier = OneVsRestClassifier(self.model(**kwargs))
-        self._make_targets(y) # Modifies self.targets
+        # Normally we pass targets as a list of strings
+        # but we need to process it first to do test train split
+        if y.ndim == 1:
+            self._make_targets(y) # Modifies self.targets
+        else:
+            self.targets = y
         self.classifier.fit(self.X, self.targets)
 
 
@@ -45,14 +53,20 @@ class MultiTargetModel():
         """
         Convert column of stringlists (e.g., '1,12,E') to MultiLabelBinarizer
         """
-        mlb = MultiLabelBinarizer()
+        self.mlb = MultiLabelBinarizer()
         # Type casting messed up and need to get value out of tuple
         strings = y.astype(str)
         clean = [x.replace(' ', '') for x in strings]
         dummies = [x.split(',') for x in clean]
-        self.targets = mlb.fit_transform(dummies)
-        self.target_classes = mlb.classes_
+        self.targets = self.mlb.fit_transform(dummies)
+        self.target_classes = self.mlb.classes_
         #print(self.target_classes)
+
+    def _transform_targets(self, y):
+        strings = y.astype(str)
+        clean = [x.replace(' ', '') for x in strings]
+        dummies = [x.split(',') for x in clean]
+        return self.mlb.transform(dummies)
 
     def make_predictions(self, X):
         """
@@ -60,43 +74,59 @@ class MultiTargetModel():
         self.predictions = self.classifier.predict(X)
         return self.predictions
 
-    def calc_accuracy(self, X=None):
+    def calc_accuracy(self, X=None, y=None):
         """
         """
-        if not X:
+        if X is None:
             X = self.X
-        self.accuracy = self.classifier.score(X, self.targets)
+        else:
+            X = self.vectorizer.transform(X)
+        if y is None:
+            y = self.targets
+
+        self.accuracy = self.classifier.score(X, y)
         print(self.accuracy)
 
     def calc_recall(self, X=None, y=None):
         """
         """
-        if not X:
+        if X is None:
             X = self.X
-        if not y:
+        else:
+            X = self.vectorizer.transform(X)
+        if y is None:
             y = self.targets
+
         labels = np.array(y)
         preds = np.array(self.make_predictions(X))
-        #labels[labels == 0] = 2
-        #preds[preds == 0] = 3
-        correct = ((labels == preds) and (labels == 1))
-        #pred[preds == 3] = 0
-        #labels[labels == 2] = 0
-        print(correct)
+        labels[labels == 0] = 2
+        preds[preds == 0] = 3
+        #correct = labels[(labels==1) & (preds==1)]
+        correct = labels == preds
+        preds[preds == 3] = 0
+        labels[labels == 2] = 0
         self.recall = correct.sum(axis = 0) / labels.sum(axis=0) # Labels = True Positives + False Negatives
         avg = 0
         for score in self.recall:
             print(score)
             avg += score
         print('Average Recall: {}'.format(avg/len(self.recall)))
+        weighted = 0
+        weights = labels.sum(axis=0) / labels.sum() # columnwise totals divided by total of array
+        for score, weight in zip(self.recall, weights):
+            weighted += score*weight
+        print('Average Weighted Recall: {}'.format(weighted))
 
-    def calc_preciscion(self, X=None):
+    def calc_preciscion(self, X=None, y=None):
         """
         """
-        if not X:
+        if X is None:
             X = self.X
-        if not y:
+        else:
+            X = self.vectorizer.transform(X)
+        if y is None:
             y = self.targets
+
         labels = np.array(y)
         preds = np.array(self.make_predictions(X))
         labels[labels == 0] = 2 # Hacky solution. Couldn't find a way to only evaluate
@@ -104,18 +134,18 @@ class MultiTargetModel():
         correct = labels == preds # True positives
         preds[preds == 3] = 0
         labels[labels ==2] = 0
-        self.precision = correct.sum(axis = 0) / preds.sum(axis=0) # Predictions = True Positives + False Positives
-        weights = preds.sum(axis=0) / preds.sum() # columnwise totals divided by total of array
+        self.precision = (correct.sum(axis = 0)+1) / (preds.sum(axis=0)+1) # Predictions = True Positives + False Positives
+
         avg = 0
         for score in self.precision:
             print(score)
             avg += score
         print('Average Precision: {}'.format(avg/len(self.precision)))
         weighted = 0
-        for score,weight in self.precision:
-            print(score)
-            avg += score
-        print('Average Precision: {}'.format(avg/len(self.precision)))
+        weights = labels.sum(axis=0) / labels.sum() # columnwise totals divided by total of array
+        for score, weight in zip(self.precision, weights):
+            weighted += score*weight
+        print('Average Weighted Precision: {}'.format(weighted))
 
 
 
@@ -135,11 +165,40 @@ if __name__ == '__main__':
     text = data[:,0]
     labels = data[:,1]
 
+    print('Number of documents in training db: {}'.format(len(text)))
+
     with open('../ref_analysis/data/common-english-words.csv') as f:
         stop_words = [word.strip() for word in f]
 
-    vectizer = CountVectorizer(stop_words=stop_words, tokenizer=LemmaTokenizer())
-    X = vectizer.fit_transform(text)
+    # Cleans and prepares data for test train split
+    strings = labels.astype(str)
+    clean = [x.replace(' ', '') for x in strings]
+    dummies = [x.split(',') for x in clean]
 
-    multilabler = MultiTargetModel(MultinomialNB)
-    multilabler.fit_classifier(X, labels)
+    # Create a label array
+    mlb = MultiLabelBinarizer()
+    ys = mlb.fit_transform(dummies)
+    sums = ys.sum(axis=0)
+    print('Counts of classes:\n', sums)
+    # Drop classes with fewer than 100 samples (needed to get stratified
+    # test train split data)
+    drop = []
+    for col in range(ys.shape[1]):
+        if sums[col] < 100:
+            drop.append(col)
+    # Prints shape before and after dropping so we can make sure they are
+    # actually dropping.
+    print(ys.shape)
+    ys = np.delete(ys, np.array(drop), axis=1)
+    print(ys.shape)
+
+    X_train, X_test, y_train, y_test = train_test_split(text, ys, stratify=ys)
+    model = MultiTargetModel(MultinomialNB, vectorizer=CountVectorizer, stop_words=stop_words, tokenizer=LemmaTokenizer, ngram_range=(1, 5), binary=False)
+    model.fit_classifier(X_train, y_train, alpha=0, fit_prior=False)
+
+    print('Recall:')
+    model.calc_recall(X_test, y_test)
+    print('Precision:')
+    model.calc_preciscion(X_test, y_test)
+    print('Accuracy:')
+    model.calc_accuracy(X_test, y_test)